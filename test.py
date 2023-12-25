1ff421e1c3b6119808a0ab35a67660a648370f2e
houyepu <yhou0023@student.monash.edu>
2023-12-25 15:08:18 +1100

    model complete

--- test.py
+++ test.py
@@ -1,17 +1,33 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import pandas as pd
 
-# Sample data
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie', 'Diana', 'Ethan'],
-    'Age': [25, 32, 45, 20, 35],
-    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
-}
+# Load the data from the uploaded CSV file
+file_path = 'Machine-Learning-Project\Salary.csv'
+salary_df = pd.read_csv(file_path)
 
-# Creating DataFrame
-df = pd.DataFrame(data)
+# Prepare the data
+X = salary_df[['YearsExperience']]
+y = salary_df['Salary']
 
-# Adding a new column 'Over 30'
-df['Over 30'] = df['Age'] > 30
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Display the DataFrame
-print(df)
+# Create and train the linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predictions for plotting
+y_pred = model.predict(X)
+
+# Create the regression plot
+plt.figure(figsize=(10, 6))
+sns.regplot(x='YearsExperience', y='Salary', data=salary_df, scatter_kws={"s": 50}, order=1, ci=None, scatter=True)
+plt.plot(X, y_pred, color='red') # regression line
+plt.title('Salary vs Years of Experience')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
